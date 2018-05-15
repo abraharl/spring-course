3924901cf27d3eed126a176efc6fbe20520be647
Richard Abraham <abraharl@eckerd.edu>
2018-05-14 20:53:08 -0500

    GetFortune now returns random fortune

--- src/main/java/tutorial/depencyInjection/fortune/HappyFortuneService.java
+++ src/main/java/tutorial/depencyInjection/fortune/HappyFortuneService.java
@@ -1,11 +1,19 @@
 package tutorial.depencyInjection.fortune;
 
+import java.util.Random;
+
 /**
  * Created by abrah on 5/13/2018.
  */
 public class HappyFortuneService implements FortuneService {
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        final String[] fortunes = new String[]{
+                "You will find wealth.",
+                "You will find health.",
+                "You will find happiness",
+        };
+
+        return fortunes[(new Random()).ints(0, fortunes.length).findFirst().getAsInt()];
     }
 }
